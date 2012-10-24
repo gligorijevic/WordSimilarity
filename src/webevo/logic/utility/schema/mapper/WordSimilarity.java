034842818815636367498fd541cc00054263476c
DjordjeGligorijevic90@gmail.com <DjordjeGligorijevic90@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-10-24 04:04:14 +0000

    word similarity

---
+++ src/webevo/logic/utility/schema/mapper/WordSimilarity.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webevo.logic.utility.schema.mapper;
+
+/**
+ *
+ * @author Djordje
+ */
+public class WordSimilarity {
+    
+    public static int measureSimilarity(String a, String b) {
+        //TODO: implement
+        
+        return 0;
+    }
+    
+}
